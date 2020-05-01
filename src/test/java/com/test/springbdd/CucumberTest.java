3b551235e472479be48bffaae95a0badb547a88e
Krushna Dash <Krushnachandra.Dash@walmartlabs.com>
2020-05-01 17:13:29 +0530

    Cucumber report plugin added for HTML and prettey report

--- src/test/java/com/test/springbdd/CucumberTest.java
+++ src/test/java/com/test/springbdd/CucumberTest.java
@@ -6,7 +6,8 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/")
+@CucumberOptions(features = "src/test/resources/", plugin = { "json:target/cucumber.json", "pretty",
+		"html:target/cucumber-reports" })
 public class CucumberTest {
 
 }
